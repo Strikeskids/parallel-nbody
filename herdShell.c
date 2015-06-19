dd6b9d502feaaded3384386487c15e0b1688312f
cdeboor <2015cdeboor@tjhsst.edu>
2015-06-19 15:15:01 -0400

    Better herding technique (forces)

--- herdShell.c
+++ herdShell.c
@@ -15,12 +15,14 @@ typedef struct {
     double x ;
     double y ;
     //
-    double v ;
-    double t ;
-    //
-    double dv ;
-    double dt ;
-    //
+    double vx;
+    double vy;
+
+    double dv;
+    double dt;
+
+    double ax;
+    double ay;
 } Node; 
 
 typedef struct {
@@ -60,13 +62,15 @@ double myrand()
 {
     return ( 1.0 * rand() ) / RAND_MAX ;
 } 
+double sqmag(double dx, double dy) {
+    return sqrt(dx*dx + dy*dy);
+}
+
 // 
-double mydist( double x1 , double y1 , double x2 , double y2 )
-{
+double mydist( double x1 , double y1 , double x2 , double y2 ) {
     double dx = x2 - x1 ;
     double dy = y2 - y1 ;
-    //
-    return sqrt( dx*dx + dy*dy ) ;
+    return sqmag(dx, dy);
 } 
 // 
 void idlefunc(void)
@@ -77,39 +81,23 @@ void idlefunc(void)
     //
     for( j = 0 ; j < N ; j++ )
     {
-        arr[j].x += ( arr[j].v * cos( arr[j].t ) * DT ) ;
-        arr[j].y += ( arr[j].v * sin( arr[j].t ) * DT ) ;
+        arr[j].x += ( arr[j].vx * DT ) ;
+        arr[j].y += ( arr[j].vy * DT ) ;
         //
         // bounce
         //
-        if( arr[j].y - R < 0.0 )
-        {
+        if ( arr[j].y - R < 0.0 ) {
             arr[j].y = R ;
-            //
-            arr[j].t *= -1.0        ;
-            arr[j].t +=  2.0 * M_PI ;
-        }
-        else if( arr[j].y + R > 1.0 )
-        {
+            arr[j].vy = -arr[j].vy;
+        } else if ( arr[j].y + R > 1.0 ) {
             arr[j].y = 1.0 - R ;
-            //
-            arr[j].t *= -1.0        ;
-            arr[j].t +=  2.0 * M_PI ;
-        }
-        //
-        if( arr[j].x - R < 0.0 )
-        {
+            arr[j].vy = -arr[j].vy;
+        } if( arr[j].x - R < 0.0 ) {
             arr[j].x = R ;
-            //
-            arr[j].t *= -1.0        ;
-            arr[j].t +=  1.0 * M_PI ;
-        }
-        else if( arr[j].x + R > 1.0 )
-        {
+            arr[j].vx = -arr[j].vx;
+        } else if( arr[j].x + R > 1.0 ) {
             arr[j].x = 1.0 - R ;
-            //
-            arr[j].t *= -1.0        ;
-            arr[j].t +=  1.0 * M_PI ;
+            arr[j].vx = -arr[j].vx;
         }
     }
     //
@@ -135,74 +123,74 @@ void idlefunc(void)
         //
         nbr[j] = kmin ;
     }
-    //
-    //
-    //
-    float changeFactor = 0.1;
+
+
+    float changeFactor = 0.5;
     float influenceFactor = 0.7;
-    for( j = 0 ; j < N ; j++ ) {
+    for (j=0; j<N; ++j) {
         k = nbr[j];
-        //
-        double dv = arr[k].v - arr[j].v;
-        double dt = arr[k].t - arr[j].t;
+        double dv = sqmag(arr[k].vx, arr[k].vy) - sqmag(arr[j].vx, arr[j].vy);
+        double dt = atan2(arr[k].vy, arr[k].vx) - atan2(arr[j].vy, arr[j].vx);
         if (dt < -M_PI) {
             dt += 2*M_PI;
         } else if (dt > M_PI) {
             dt -= 2*M_PI;
         }
-
         arr[j].dv = changeFactor * (dv * influenceFactor * myrand() - dv * (1-influenceFactor));
         arr[j].dt = changeFactor * (dt * influenceFactor * myrand() - dt * (1-influenceFactor));
+        arr[j].ax = 0;
+        arr[j].ay = 0;
+    }
 
-        double dist = mydist(arr[k].x, arr[k].y, arr[j].x, arr[j].y);
-
-        if (dist < 3*R) {
-            double dx = arr[k].x - arr[j].x;
-            double dy = arr[k].y - arr[j].y;
-            
-            dist = 3*R-dist;
-
-            double forceAngle = atan2(-dy, -dx);
-            double forceMag = dist * dist;
-            
-            arr[j].dv += forceMag * 100 * cos(forceAngle - arr[j].t);
-            double forceDt = forceAngle - arr[j].t;
-            if (forceDt < -M_PI) forceDt += 2*M_PI;
-            else if (forceDt > M_PI) forceDt -= 2*M_PI;
-            arr[j].dt += changeFactor * forceDt * 0.3;
+    double forceFactor = 100000; // How strong overlaps are
+    for (j=0; j<N; ++j) {
+        k = nbr[j];
+        double dx = arr[k].x - arr[j].x;
+        double dy = arr[k].y - arr[j].y;
+        double dist = sqmag(dx, dy);
+        double overlap = 2*R-dist;
+        if (overlap > 0 && (j < k || nbr[k] != j)) { // Hack to compute 3rd law efficiently
+            double forceMag = forceFactor * overlap * overlap / R * R;
+            double ax = forceMag * dx / dist;
+            double ay = forceMag * dy / dist;
+            arr[j].ax -= ax;
+            arr[j].ay -= ay;
+            arr[k].ax += ax;
+            arr[k].ay += ay;
         }
+    }
 
-        for (k=0;k<O;++k) {
-            double dist = mydist(arr[j].x, arr[j].y, obs[k].x, obs[k].y);
-
-            if (dist < 2*R + obs[k].r) {
-                double dx = obs[k].x - arr[j].x;
-                double dy = obs[k].y - arr[j].y;
-                
-                dist = 2*R+obs[k].r-dist;
-
-                double forceAngle = atan2(-dy, -dx);
-                double forceMag = dist * dist;
-                
-                arr[j].dv += forceMag * 1000 * cos(forceAngle - arr[j].t);
-                double forceDt = forceAngle - arr[j].t;
-                if (forceDt < -M_PI) forceDt += 2*M_PI;
-                else if (forceDt > M_PI) forceDt -= 2*M_PI;
-                arr[j].dt += changeFactor * forceDt * 0.8;
-                break;
+    for (j=0; j<N; ++j) {
+        for (k=0; k<O; ++k) {
+            double dx = obs[k].x - arr[j].x;
+            double dy = obs[k].y - arr[j].y;
+            double dist = sqmag(dx, dy);
+            double overlap = R + R + obs[k].r - dist;
+            if (overlap > 0) { // Hack to compute 3rd law efficiently
+                double forceMag = forceFactor * overlap * overlap / R * R;
+                double ax = forceMag * dx / dist;
+                double ay = forceMag * dy / dist;
+                arr[j].ax -= ax;
+                arr[j].ay -= ay;
             }
         }
     }
-    //
-    for( j = 0 ; j < N ; j++ )
-    {
-        arr[j].v += arr[j].dv ;
-        if( arr[j].v > 0.15 ) arr[j].v = 0.15 ;
-        if( arr[j].v < 0.05 ) arr[j].v = 0.05 ;
-        //
-        arr[j].t += arr[j].dt ;
-        if( arr[j].t > 2.0 * M_PI ) arr[j].t -= ( 2.0 * M_PI ) ;
-        if( arr[j].t < 0.0        ) arr[j].t += ( 2.0 * M_PI ) ;
+
+    for (j=0; j<N; ++j) {
+        if (abs(arr[j].ax) > 2) arr[j].ax = 2 * arr[j].ax / abs(arr[j].ax);
+        if (abs(arr[j].ay) > 2) arr[j].ay = 2 * arr[j].ay / abs(arr[j].ay);
+    }
+
+    for( j = 0 ; j < N ; j++ ) {
+        double oldv = sqmag(arr[j].vx, arr[j].vy);
+        double newv = oldv + arr[j].dv;
+        if( newv > 0.15 ) newv = 0.15 ;
+        if( newv < 0.05 ) newv = 0.05 ;
+
+        double oldt = atan2(arr[j].vy, arr[j].vx);
+        double newt = oldt + arr[j].dt;
+        arr[j].vx = cos(newt) * newv + DT * arr[j].ax;
+        arr[j].vy = sin(newt) * newv + DT * arr[j].ay;
     }
     //
     glutPostRedisplay() ;
@@ -293,7 +281,7 @@ void displayfunc(void)
         //
         x = arr[j].x ;
         y = arr[j].y ;
-        t = arr[j].t ;
+        t = atan2(arr[j].vy, arr[j].vx) ;
         //
         x1 = x + R * cos( t + 0.5 * M_PI ) ;
         y1 = y + R * sin( t + 0.5 * M_PI ) ;
@@ -365,8 +353,8 @@ int main(int argc,char* argv[])
         arr[j].x = myrand() ;
         arr[j].y = myrand() ;
         //
-        arr[j].v = myrand() * 0.1 + 0.05 ;
-        arr[j].t = myrand() * 2.0 * M_PI ;
+        arr[j].vx = myrand() * 0.1 + 0.05 ;
+        arr[j].vy = myrand() * 0.1 + 0.05 ;
     }
 
 //    for (j=0;j<O;++j) {
